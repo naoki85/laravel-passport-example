1a0879675d7e791e11b99223e78aca8c08342626
naoki85 <naoki.yoneyama.85@gmail.com>
2021-08-14 23:27:37 +0900

    Add web container

---
+++ web/about.php
@@ -0,0 +1,10 @@
+<html lang="ja">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    <title>Home | Laravel Passport Example</title>
+  </head>
+  <body>
+    <h1>About</h1>
+  </body>
+</html>
---
+++ web/index.php
@@ -0,0 +1,4 @@
+<?php
+$request_uri = $_SERVER['REQUEST_URI'];
+require_once('.'.$request_uri.'.php');
+?>
