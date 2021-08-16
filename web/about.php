1770b355c86d187d0a2e5bf48be98c7deccb3166
naoki85 <naoki.yoneyama.85@gmail.com>
2021-08-16 22:27:13 +0900

    get user_id from session

--- web/about.php
+++ web/about.php
@@ -1,3 +1,31 @@
+<?php
+define('SESSION_COOKIE_NAME', 'laravel_session');
+define('MEMCACHED_HOST_NAME', 'memcached');
+define('LARAVEL_SESSION_PREFIX', 'laravel_cache:');
+define('LARAVEL_CACHE_PREFIX', 'login_web_');
+
+function getUserIdFromSession(string $session): int | null {
+  $filtered = array_filter(unserialize($session), function($k) {
+    return strpos($k, LARAVEL_CACHE_PREFIX) !== false;
+  }, ARRAY_FILTER_USE_KEY);
+  if (empty($filtered)) return null;
+
+  return (int) array_values($filtered)[0];
+}
+
+
+$memcached = new Memcached();
+$memcached->addServer(MEMCACHED_HOST_NAME, 11211);
+
+$user_id = null;
+if (!empty($_COOKIE[SESSION_COOKIE_NAME])) {
+  $session = $memcached->get(LARAVEL_SESSION_PREFIX.$_COOKIE[SESSION_COOKIE_NAME]);
+  if (!empty($session)) {
+    $user_id = getUserIdFromSession($session);
+  }
+}
+?>
+
 <html lang="ja">
   <head>
     <meta charset="utf-8" />
@@ -6,5 +34,10 @@
   </head>
   <body>
     <h1>About</h1>
+    <?php if ($user_id): ?>
+      <p>Your ID: <?= $user_id ?></p>
+    <? else: ?>
+      <p>You are not logged in.</p>
+    <?php endif; ?>
   </body>
 </html>
--- web/index.php
+++ web/index.php
@@ -1,4 +1,14 @@
 <?php
 $request_uri = $_SERVER['REQUEST_URI'];
-require_once('.'.$request_uri.'.php');
+if ($request_uri === '/' || $request_uri === 'index') {
+  return;
+}
+
+$basename = basename($request_uri);
+if (!file_exists('./'.$basename.'.php')) {
+  header('HTTP/1.1 404 Not Found');
+  exit();
+}
+
+require_once('./'.$basename.'.php');
 ?>
