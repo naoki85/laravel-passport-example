7c055099602468989366b567c570fffaeeef56b6
naoki85 <naoki.yoneyama.85@gmail.com>
2021-08-16 23:27:58 +0900

    #2 set loading params

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link'
 import Layout from '../components/Layout'
 import { useEffect, useState } from 'react'
 import { User } from '../interfaces'
@@ -6,25 +5,32 @@ import fetchJson from '../lib/fetchJson'
 
 const IndexPage = () => {
   const [user, setUser] = useState<User>(null)
+  const [loading, setLoading] = useState<boolean>(false)
 
   useEffect(() => {
     (async function () {
       try {
+        setLoading(true)
         const response = await fetchJson('/api/users')
         setUser(response.user)
       } catch (err) {
         console.error(err)
         setUser(null)
+      } finally {
+        setLoading(false)
       }
     })()
   }, [])
 
   return (
     <Layout title="Home | Next.js + Laravel Passport Example">
+      {loading && (
+        <p>Loading</p>
+      )}
       {user ? (
         <h1>{`Hello ${user.name} 👋`}</h1>
       ) : (
-        <p>Loading</p>
+        <p>You are not logged in. <a href="http://localhost:8080/login">Please login</a></p>
       )}
     </Layout>
   )
